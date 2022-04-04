0effaf2e6afc553f889f6cc9044c849c1c634f36
MartinAlcaraz <96241107+MartinAlcaraz@users.noreply.github.com>
2022-04-04 19:29:56 -0300

    campo nombre de formulario terminado

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 import { validar } from "./validaciones.js";
 
-const inputs= document.querySelectorAll(".input");
+const inputs = document.querySelectorAll(".input");
 const enviar = document.querySelector("[data-enviar]");
 
-inputs.forEach( (input) => {
+
+inputs.forEach((input) => {
     //console.log(input.attributes.name);
+    input.setCustomValidity("El campo está vacío"); 
+
     input.addEventListener("blur", (input) => {
-        validar(input.target) });
+        validar(input.target)
+    });
+    input.addEventListener("keyup", (input) => {    // comprueba despues de ingresar un caracter del teclado
+        validar(input.target);
+    });
+
+
 });
 
 enviar.addEventListener("click", (event) => {
-    event.preventDefault();
-    
-    
-})
+    //event.preventDefault(); 
+});
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,22 +1,21 @@
 
 export const validar = (input) => {
-    const tipoDeInput= (input.dataset.tipo);
+    const tipoDeInput = (input.dataset.tipo);
 
-    if(validadores[tipoDeInput]){
+    if (validadores[tipoDeInput]) {
         validadores[tipoDeInput](input);
     }
-    
-    //input.setCustomValidity("");  
-      // para comprobar si un input es valido hay que setear setCustomValidity("")
-     // con string vacío ""        
+    // para comprobar si un input es valido hay que setear setCustomValidity("")
+    // con string vacío ""        
     // para que No se muestren los mensajes de error (por defecto) al
-      //  enviar el formulario con datos incorrectos, hay que setear 
-      //el setCustomValidity con un char espacio (" ") o con algun string
-    if (input.validity.valid){
+    //  enviar el formulario con datos incorrectos, hay que setear 
+    //el setCustomValidity con un char espacio (" ") o con algun string
+
+    if (input.validity.valid) {
+        input.setCustomValidity(" ");
         input.parentElement.querySelector(".message-error").innerHTML = "";
         input.parentElement.classList.remove("input__invalid");
-    }else{
-        
+    } else {
         input.parentElement.classList.add("input__invalid");
         let mensajeDeError = mostrarMensajeDeError(input, tipoDeInput);
         input.parentElement.querySelector(".message-error").innerHTML = mensajeDeError;
@@ -27,22 +26,32 @@ const validadores = {
     nombre: (input) => validarNombre(input)
 }
 
-function validarNombre(input){                                                                  
-     //  para que input.checkValidity() no arroje un false y compruebe si es valido
+function validarNombre(input) {
+    //  para que input.checkValidity() no arroje un false y compruebe si es valido
     //  el campo primero hay que setear el setCustomValidity("") en vacío, string vacío.                 
     let mensaje = "";
-    if (input.value == ""){
-        mensaje = "el campo esta vacio";
-    }   
-    input.setCustomValidity(mensaje); 
+    
+    let texto = input.value.trim();
+
+    if (texto.length < 3) {
+        mensaje = "El nombre tiene que tener al menos 3 caracteres."
+    } else {
+        if (texto.length > 50){
+            mensaje = "El nombre tiene que tener menos de 50 caracteres."
+        }
+    }
+    input.setCustomValidity(mensaje);
 }
+ 
+    
+
 
 const tipoDeErrores = [     // vector con los tipos de errores que pueden ocurrir
     "valueMissing",
     "typeMismatch",
     "patternMismatch",
     "tooLong",
-    "tooShort"        
+    "tooShort"
 ];
 
 const mensajeDeErrores = {
@@ -62,15 +71,15 @@ const mensajeDeErrores = {
     }
 }
 
-function mostrarMensajeDeError(input, tipoDeInput){
-    let mensajes="";
-    tipoDeErrores.forEach( (error) => {
-        if(input.validity[error]){
+function mostrarMensajeDeError(input, tipoDeInput) {
+    let mensajes = "";
+    tipoDeErrores.forEach((error) => {
+        if (input.validity[error]) {
             console.log("..error tipo: ", error);
             console.log(mensajeDeErrores[tipoDeInput][error]);
             mensajes += mensajeDeErrores[tipoDeInput][error];
         }
-        
+
     });
     return mensajes;
 
